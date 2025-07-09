c6d840b2b3b802050fd9f95317379d89163bcb37
Mihail Neronov <redsneakerstore@gmail.com>
2025-07-09 08:48:19 +0400

    Fix: enable headless mode for CI

--- conftest.py
+++ conftest.py
@@ -1,11 +1,10 @@
 import pytest
-from playwright.sync_api import sync_playwright
 
-@pytest.fixture(scope="function")
-def page():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        yield page
-        browser.close()
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {"width": 1280, "height": 720},
+        "screen": {"width": 1280, "height": 720},
+        "headless": True
+    }
