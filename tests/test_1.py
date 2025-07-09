c2c8e2f1e71e170b54bc73f15a502a8d35dc89f5
Mihail Neronov <redsneakerstore@gmail.com>
2025-07-09 09:03:58 +0400

    Add Allure integration and fix test_3

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,14 +1,18 @@
-import re
-import pytest
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect
 
+def test_run():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
 
-def test_run(page):
-    page.goto("https://jrnys.com/")
-    page.get_by_test_id("login-icon").click()
-    page.get_by_test_id("login-email-input").fill("invalidemail@mail.com")
-    page.get_by_test_id("login-password-input").fill("password12")
-    page.get_by_test_id("login-submit-button").click()
+        page.goto("https://jrnys.com/")
+        page.get_by_test_id("login-icon").click()
+        page.get_by_test_id("login-email-input").fill("invalidemail@mail.com")
+        page.get_by_test_id("login-password-input").fill("password12")
+        page.get_by_test_id("login-submit-button").click()
 
-    expect(page.get_by_text("Invalid username or password.", exact=True)).to_be_visible()
+        expect(page.get_by_text("Invalid username or password.", exact=True)).to_be_visible()
 
+        context.close()
+        browser.close()
--- tests/test_2.py
+++ tests/test_2.py
@@ -1,13 +1,17 @@
-import re
-import pytest
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect
 
+def test_run():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
 
-def test_run(page):
-    page.goto("https://jrnys.com/")
-    page.get_by_test_id("login-icon").click()
-    page.get_by_test_id("login-email-input").fill("invalid1")
-    page.get_by_test_id("login-submit-button").click()
+        page.goto("https://jrnys.com/")
+        page.get_by_test_id("login-icon").click()
+        page.get_by_test_id("login-email-input").fill("invalid1")
+        page.get_by_test_id("login-submit-button").click()
 
-    expect(page.get_by_test_id("login-email-error-message")).to_have_text("Value is not a valid email address")
+        expect(page.get_by_test_id("login-email-error-message")).to_have_text("Value is not a valid email address")
 
+        context.close()
+        browser.close()
